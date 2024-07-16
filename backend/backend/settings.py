aee574c34ae776e5f33ae1a6b6f7e8689f81090f
“monemonic” <monemonic95@yandex.ru>
2024-07-16 16:59:08 +0300

    fix admin

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1 localhost").split()
 
+CSRF_TRUSTED_ORIGINS = ['https://homeworkenkorolev.zapto.org']
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
